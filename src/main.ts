5c8df1ee4b400bb9bf728d9534f91427e55ecc71
Yusuf <ay.techbuilds@gmail.com>
2024-01-26 05:49:17 +0100

    update cors Policy

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:5173',
-        'https://seismic-demo.aycodes.live',
-      ];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    methods: '*',
     allowedHeaders: 'Content-Type,Authorization',
-    credentials: true, // Keep this only if your front end needs to send credentials with the request
+    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
